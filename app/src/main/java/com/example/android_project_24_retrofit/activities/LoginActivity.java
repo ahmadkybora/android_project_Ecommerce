b17b5d9bf619015e3f92df193bce69ec5c2d9342
kybora <amontazeri53@gmail.com>
2021-09-02 11:39:43 +0430

    mew

---
+++ app/src/main/java/com/example/android_project_24_retrofit/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.android_project_24_retrofit.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.android_project_24_retrofit.R;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/android_project_24_retrofit/activities/RegisterActivity2.java
@@ -0,0 +1,14 @@
+package com.example.android_project_24_retrofit.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.android_project_24_retrofit.R;
+public class RegisterActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
